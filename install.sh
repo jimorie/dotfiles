7f6519d2bdee9eb8387661c9b6af8bfff7a8b54d
Petter Nyström <knowit-pnystrom@op5.com>
2018-01-15 16:32:33 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+rm -i ~/.bash_profile
+ln -s bash_profile ~/.bash_profile
+
+rm -i ~/.git-completion.bash
+ln -s git-completion.bash ~/.git-completion.bash
+
+rm -i ~/.inputrc
+ln -s inputrc ~/.inputrc
+
+rm -i ~/.tmux.conf
+ln -s tmux.conf ~/.tmux.conf
+
+rm -ir ~/.vim
+ln -s vim  ~/.vim
+
+rm -i ~/.vimrc
+ln -s vimrc ~/.vimrc
