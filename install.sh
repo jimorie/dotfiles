f6e5a3aaa2b0452386298af1b498b657ced6de67
Petter Nyström <jimorie@gmail.com>
2018-01-15 16:43:49 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 rm -i ~/.bash_profile
-ln -s bash_profile ~/.bash_profile
+ln -s `pwd`/bash_profile ~/.bash_profile
 
 rm -i ~/.git-completion.bash
-ln -s git-completion.bash ~/.git-completion.bash
+ln -s `pwd`/git-completion.bash ~/.git-completion.bash
 
 rm -i ~/.inputrc
-ln -s inputrc ~/.inputrc
+ln -s `pwd`/inputrc ~/.inputrc
 
 rm -i ~/.tmux.conf
-ln -s tmux.conf ~/.tmux.conf
+ln -s `pwd`/tmux.conf ~/.tmux.conf
 
 rm -ir ~/.vim
-ln -s vim  ~/.vim
+ln -s `pwd`/vim  ~/.vim
 
 rm -i ~/.vimrc
-ln -s vimrc ~/.vimrc
+ln -s `pwd`/vim/vimrc ~/.vimrc
