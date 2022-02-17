8626259e7c24b0fd51c1f230e0ff2ceb894741d8
Petter Nyström <pnystrom@regifted.local>
2022-02-17 11:48:45 +0100

    Add st4 User config

--- install.sh
+++ install.sh
@@ -45,3 +45,12 @@ fi
 
 rm -f $dir/.gitignore
 ln -s `pwd`/gitignore $dir/.gitignore
+
+if [ ! -d "$dir/Library/Application\ Support/Sublime\ Text/Packages/User" ]; then
+    if [ ! -f "$dir/Library/Application Support/Sublime Text/Packages/User.orig" ]; then
+        mv "$dir/Library/Application Support/Sublime Text/Packages/User" "$dir/Library/Application Support/Sublime Text/Packages/User.orig" 
+    else
+        rm -rf "$dir/Library/Application Support/Sublime Text/Packages/User"
+    fi
+    ln -s "`pwd`/st4/User" "$dir/Library/Application Support/Sublime Text/Packages/User"
+fi
