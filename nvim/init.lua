2302e8c21b3a3252fd6803976307c39d2a34b8e7
Petter Nyström <jimorie@gmail.com>
2025-02-25 10:42:42 +0000

    nvim: Add nvim-surround plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -216,6 +216,18 @@ require('lazy').setup({
       'nvim-tree/nvim-web-devicons',
     },
   },
+
+	{
+			"kylechui/nvim-surround",
+			version = "*", -- Use for stability; omit to use `main` branch for the latest features
+			event = "VeryLazy",
+			config = function()
+					require("nvim-surround").setup({
+            move_cursor = "sticky",
+					})
+			end
+	},
+
 }, {})
 
 -- [[ Setting options ]]
