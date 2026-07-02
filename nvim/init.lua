c81efeced5e0835b90e407aff91af2586c70ac8a
Petter Nyström <jimorie@gmail.com>
2026-07-02 16:05:35 +0200

    nvim: Fix yank deprecation warning

--- nvim/init.lua
+++ nvim/init.lua
@@ -299,7 +299,7 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    vim.hl.on_yank()
+    vim.hl.hl_op()
   end,
   group = highlight_group,
   pattern = '*',
